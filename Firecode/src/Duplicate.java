434f9bee2d38bb3ec7e67aac90cc3a32aaa880d2
ayseguldalkiran <aysegul.dalkiran.98@gmail.com>
2021-01-04 21:22:48 +0300

    Duplicate

--- Firecode/src/Duplicate.java
+++ Firecode/src/Duplicate.java
@@ -1,14 +1,16 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Duplicate {
     public static void main(String[] args) {
-        int[] numbers = {1,3,4,2,1,2,4};
+        int[] numbers = {1,3,4,2,1,4,2};
         System.out.println(duplicate(numbers));
     }
     public static String duplicate(int[] numbers){
         //duplicate({1,3,4,2,1}) --> "[1]"
         //duplicate({1,3,4,2,1,2,4}) --> "[1, 2, 4]"
+        ArrayList<Integer> list = new ArrayList<>();
         HashMap<Integer, Integer> map= new HashMap<>();
         for(int i=0; i<numbers.length; i++){
             if(map.containsKey(numbers[i])){
@@ -20,15 +22,15 @@ public class Duplicate {
                 map.put(numbers[i],1);
             }
         }
-        /*for(Integer x: map.keySet()){
+        for(Integer x: map.keySet()){
             if(map.get(x)!=1){
-                String str=x.toString();
-                return str;
+                list.add(x);
             }
-        }*/
 
+        }
+        String str = list.toString();
+        return str;
 
-        return null;
 
     }
 }
